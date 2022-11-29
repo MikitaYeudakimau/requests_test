3bd15a5e878337c96263497a8d1c3a3c6203fb18
Mikita Yeudakimau <nikevdokimovv@gmail.com>
2022-11-29 11:43:19 +0300

    main function is separated

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import time
+
 import requests
 import urlextract
 import re
 
 
-def parse(string):
-    dict = {}
+# http_methods = {
+#     'GET': requests.get(url),
+#     'POST': requests.post(url),
+#     'PUT': requests.put(url),
+#     'DELETE': requests.delete(url),
+#     'HEAD': requests.head(url),
+#     'PATCH': requests.patch(url),
+#     'OPTION': requests.options(url),
+# }
+def check_name(string):
+    sites = {}
     lst = [i.strip(" ") for i in string.split(",")]
     for i in lst:
         url = i
@@ -16,7 +27,10 @@ def parse(string):
         elif re.match(r'https?://', url) is None:
             print("URL введен некорректно")
             continue
-        dict[url] = {}
+        sites[url] = {}
+    return sites
+
+def check_http(dict):
     for url in dict.keys():
         response = requests.get(url)
         if response.status_code != 405:
@@ -39,12 +53,18 @@ def parse(string):
         response = requests.options(url)
         if response.status_code != 405:
             dict[url]['OPTIONS'] = response.status_code
+    return dict
+
+def parse(string):
+    t1 = time.time()
+    sites = check_name(string)
+    dict = check_http(sites)
+    t2 = time.time()
+    dt = t2 - t1
     if dict:
         print(dict)
+        print(dt)
 
-if  __name__ == '__main__':
+
+if __name__ == '__main__':
     parse(input("Введите URL через запятую: "))
-
-
-
-
