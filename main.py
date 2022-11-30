e9c86f9334f06f0b8ae341909cc5b62e585d583b
Mikita Yeudakimau <nikevdokimovv@gmail.com>
2022-11-30 13:25:30 +0300

    initial attemps

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import asyncio
 import time
-
 import requests
 import urlextract
 import re
 
 
 def check_name(string):
-    sites = {}
+    sites = []
     lst = [i.strip(" ") for i in string.split(",")]
     for i in lst:
         url = i
@@ -18,26 +18,28 @@ def check_name(string):
         elif re.match(r'https?://', url) is None:
             print("URL is written uncorrectly")
             continue
-        sites[url] = {}
+        sites.append(url)
     return sites
 
 
-def check_response_status(url, method, func):
+async def check_response_status(url, method, func):
     if method == "GET":
         try:
             response = func(url, 5)
+            # return response.status_code
+            print(response.status_code)
         except:
             print(f"Check '{url}' for right spelling ")
-            return False
-    else:
-        response = func(url)
-        if response.status_code != 405:
-            return response.status_code
-        else:
-            return None
+            print('False')
+    # else:
+    #     response = func(url)
+    #     if response.status_code != 405:
+    #         return response.status_code
+    #     else:
+    #         return None
 
 
-def check_http(dict):
+async def check_http(url):
     http_methods = {
         'GET': lambda url, timeout: requests.get(url, timeout=timeout),
         'POST': lambda url: requests.post(url),
@@ -47,26 +49,30 @@ def check_http(dict):
         'PATCH': lambda url: requests.patch(url),
         'OPTION': lambda url: requests.options(url),
     }
-    for url in dict.keys():
-        for method, func in http_methods.items():
-            response_status = check_response_status(url, method, func)
-            if response_status is False:
-                break
-            elif response_status is None:
-                continue
-            dict[url][method] = response_status
-    return dict
+    response_tasks = []
+    for method, func in http_methods.items():
+        response_status = asyncio.create_task(check_response_status(url, method, func))
+        response_tasks.append(response_status)
+    result = await asyncio.gather(*response_tasks)
+    print(result)
+    # if response_status is False:break
+    # elif response_status is True:
+    #     dict[url][method] = response_status
+    # print(dict)
 
 
-def parse(string):
+async def parse(string):
     t1 = time.time()
     sites = check_name(string)
-    dict = check_http(sites)
-    checked_dict = {k: v for k, v in dict.items() if v != {}}
-    if checked_dict:
-        print(checked_dict)
-        print(f"Runtime time: {time.time() - t1}")
+    print(sites)
+    tasks = []
+    for site in sites:
+        task = asyncio.create_task(check_http(site))
+        tasks.append(task)
+    await asyncio.gather(tasks)
+    # checked_dict = {k: v for k, v in dict.items() if v != {}}
+    print(f"Runtime time: {time.time() - t1}")
 
 
 if __name__ == '__main__':
-    parse(input("Введите URL через запятую: "))
+    asyncio.run(parse(input("Введите URL через запятую: ")))
