734aaa2e97db7e40059a959dbf63492d9658c5e8
Mikita Yeudakimau <nikevdokimovv@gmail.com>
2022-12-01 00:42:52 +0300

    need to upgrade def check_to_response

--- main.py
+++ main.py
@@ -19,29 +19,29 @@ def check_name(string):
             print("URL is written uncorrectly")
             continue
         sites.append(url)
+    print(sites)
     return sites
 
 
 async def check_response_status(url, method, func):
-    if method == "GET":
-        try:
-            response = func(url, 5)
-            # return response.status_code
-            print(response.status_code)
-        except:
-            print(f"Check '{url}' for right spelling ")
-            print('False')
+    # if method == "GET":
+    #     try:
+    #         response = await func(url, 5)
+    #         # return response.status_code
+    #         print(response.status_code)
+    #     except:
+    #         print(f"Check '{url}' for right spelling ")
+    #         print('False')
     # else:
-    #     response = func(url)
-    #     if response.status_code != 405:
-    #         return response.status_code
-    #     else:
-    #         return None
+    response = func(url)
+    print(f"Check method {method}")
+    if response.status_code != 405:
+        return (method, response.status_code)
 
 
 async def check_http(url):
     http_methods = {
-        'GET': lambda url, timeout: requests.get(url, timeout=timeout),
+        'GET': lambda url: requests.get(url),
         'POST': lambda url: requests.post(url),
         'PUT': lambda url: requests.put(url),
         'DELETE': lambda url: requests.delete(url),
@@ -54,25 +54,29 @@ async def check_http(url):
         response_status = asyncio.create_task(check_response_status(url, method, func))
         response_tasks.append(response_status)
     result = await asyncio.gather(*response_tasks)
+    allowed_methods = {}
     print(result)
-    # if response_status is False:break
-    # elif response_status is True:
-    #     dict[url][method] = response_status
-    # print(dict)
+    for res in result:
+        if res != None:
+            allowed_methods[res[0]] = res[1]
+    dict = {}
+    dict[url] = allowed_methods
+    return dict
+    print(dict)
 
 
-async def parse(string):
-    t1 = time.time()
-    sites = check_name(string)
-    print(sites)
+async def parse(sites):
     tasks = []
     for site in sites:
         task = asyncio.create_task(check_http(site))
         tasks.append(task)
-    await asyncio.gather(tasks)
-    # checked_dict = {k: v for k, v in dict.items() if v != {}}
-    print(f"Runtime time: {time.time() - t1}")
+    res = await asyncio.gather(*tasks)
+    print(res)
 
 
 if __name__ == '__main__':
-    asyncio.run(parse(input("Введите URL через запятую: ")))
+    sites = check_name(input("Введите URL через запятую: "))
+    t1 = time.time()
+    asyncio.run(parse(sites))
+    elapsed = time.time() - t1
+    print(f"Runtime time: {elapsed}")
