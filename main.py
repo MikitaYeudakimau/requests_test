c25a6faf20a9ac6a1fad2b231aa03bfeeed7797b
Mikita Yeudakimau <nikevdokimovv@gmail.com>
2022-12-01 15:21:25 +0300

    add aiohttp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import aiohttp
 import asyncio
 import time
-import requests
 import urlextract
 import re
 
@@ -23,46 +23,28 @@ def check_name(string):
     return sites
 
 
-async def check_response_status(url, method, func):
-    # if method == "GET":
-    #     try:
-    #         response = await func(url, 5)
-    #         # return response.status_code
-    #         print(response.status_code)
-    #     except:
-    #         print(f"Check '{url}' for right spelling ")
-    #         print('False')
-    # else:
-    response = func(url)
-    print(f"Check method {method}")
-    if response.status_code != 405:
-        return (method, response.status_code)
-
-
 async def check_http(url):
-    http_methods = {
-        'GET': lambda url: requests.get(url),
-        'POST': lambda url: requests.post(url),
-        'PUT': lambda url: requests.put(url),
-        'DELETE': lambda url: requests.delete(url),
-        'HEAD': lambda url: requests.head(url),
-        'PATCH': lambda url: requests.patch(url),
-        'OPTION': lambda url: requests.options(url),
-    }
-    response_tasks = []
-    for method, func in http_methods.items():
-        response_status = asyncio.create_task(check_response_status(url, method, func))
-        response_tasks.append(response_status)
-    result = await asyncio.gather(*response_tasks)
     allowed_methods = {}
-    print(result)
-    for res in result:
-        if res != None:
-            allowed_methods[res[0]] = res[1]
-    dict = {}
-    dict[url] = allowed_methods
-    return dict
-    print(dict)
+    async with aiohttp.ClientSession() as session:
+        http_methods = {
+            'GET': lambda url: session.get(url),
+            'POST': lambda url: session.post(url),
+            'PUT': lambda url: session.put(url),
+            'DELETE': lambda url: session.delete(url),
+            'HEAD': lambda url: session.head(url),
+            'PATCH': lambda url: session.patch(url),
+            'OPTION': lambda url: session.options(url),
+        }
+        for method, func in http_methods.items():
+            response = await func(url)
+            status = response.status
+            print(status)
+            if status != 405:
+                allowed_methods[method] = status
+        dict = {}
+        dict[url] = allowed_methods
+        print(dict)
+        return dict
 
 
 async def parse(sites):
